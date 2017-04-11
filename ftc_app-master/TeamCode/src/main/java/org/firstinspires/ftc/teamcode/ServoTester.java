ee406f957137abbcf462914f29545e05374e9866
HASAN\Hasan <hasanliou@yahoo.com>
2017-04-11 16:03:15 -0700

    breadservo

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
@@ -1,8 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Hasan on 4/11/2017.
  */
 
-public class ServoTester {
+public class ServoTester extends OpMode{
+    Servo servo360;
+    Servo highSpeed;
+    @Override
+    public void init() {
+        servo360 = hardwareMap.servo.get("floodGate");
+        highSpeed = hardwareMap.servo.get("button");
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.x) {
+            servo360.setPosition(1);
+        } else {
+            servo360.setPosition(0);
+        }
+
+        if(gamepad1.y) {
+            highSpeed.setPosition(1);
+        } else {
+            highSpeed.setPosition(0);
+        }
+    }
 }
