1363c07681a6c86f3cf7b50a2662929ea7bab1fd
Jesnine Erillo <jesninenica@yahoo.com>
2018-10-13 10:46:52 -0800

    Revert "Merge branch 'master' of https://github.com/yunghas/CarTrack"
    This reverts commit 344edfaafd8402e89576ff0007652249b767b908, reversing
    changes made to 1ea64bf77ba37dd97dfc87b523d2575b0d19db1f.

--- app/src/main/java/com/example/menga/imagerec/color.java
+++ app/src/main/java/com/example/menga/imagerec/color.java
@@ -1,6 +1,5 @@
-package com.example.menga.cartrack;
+package com.example.menga.imagerec;
 
-<<<<<<< HEAD
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -17,7 +16,6 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
@@ -26,7 +24,6 @@ import clarifai2.api.request.model.PredictRequest;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.Model;
 import clarifai2.dto.model.output.ClarifaiOutput;
-import clarifai2.dto.prediction.Color;
 import clarifai2.dto.prediction.Concept;
 import clarifai2.dto.prediction.Prediction;
 
@@ -49,30 +46,37 @@ public class color {
             image = FirebaseVisionImage.fromBitmap(stopImage);
         } catch (IOException e) {
             e.printStackTrace();
-
-    public ArrayList<com.example.menga.cartrack.entry> getData(String img){
-        final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
-        Model<Color> generalModel = client.getDefaultModels().colorModel();
-
-        PredictRequest<Color> request = generalModel.predict().withInputs(
-                ClarifaiInput.forImage(img)
-        );
-        List<ClarifaiOutput<Color>> result = request.executeSync().get();
-        ClarifaiOutput temp = result.get(0);
-        List<Prediction> tempList = temp.data();
-        ArrayList<com.example.menga.cartrack.entry> resultList = new ArrayList<>();
-        for(int i =0;i<tempList.size();i++){
-            resultList.add(new entry(tempList.get(i).asColor().webSafeColorName(),
-                    tempList.get(i).asColor().value(),tempList.get(i).asColor().webSafeHex()));
         }
-        return resultList;
-    }
 
-    public static void main(String[] args){
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        textRecognizer.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText result) {
+                        // Task completed successfully
+                        String resultText = result.getText();
+                        System.out.println(resultText);
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                System.out.println("why god");
+                            }
+                        });
+
+
+
         color colour = new color();
-        ArrayList<entry> list = colour.getData("http://pfguru.com/wp-content/uploads/2015/08/Ex-Showroom-Price-And-On-Road-Price.jpg");
-        for(int i =0;i<list.size();i++){
-            System.out.println(list.get(i).getColor()+" "+list.get(i).getValue()+" "+list.get(i).getHex());
+        List<ClarifaiOutput<Concept>> colorList = colour.getList();
+        ClarifaiOutput temp = colorList.get(0);
+        List<Prediction> tempList = temp.data();
+        for(int i =0;i<tempList.size();i++){
+            System.out.println(tempList.get(i).asConcept().name());
         }
     }
 }
