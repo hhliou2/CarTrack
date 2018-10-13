4a4c4ea5a630235a2ada7fe318ade54a89e806aa
Jesnine Erillo <jesninenica@yahoo.com>
2018-10-13 07:34:15 -0800

    Revert "Added Anthony's changes to color"
    This reverts commit e979af37e5ff31b90c63cebbf4162dab4e6cab1f.

--- app/src/main/java/com/example/menga/imagerec/color.java
+++ app/src/main/java/com/example/menga/imagerec/color.java
@@ -1,4 +1,4 @@
-package com.example.menga.colorres;
+package com.example.menga.imagerec;
 
 import java.util.List;
 
@@ -11,13 +11,13 @@ import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Concept;
 
 public class color {
-    private String image;
-    private final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
+    final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
     Model<Concept> generalModel = client.getDefaultModels().generalModel();
 
     PredictRequest<Concept> request = generalModel.predict().withInputs(
-            ClarifaiInput.forImage(image)
+            ClarifaiInput.forImage("https://www.cambridgema.gov/~/media/Images/sharedphotos/Residential-Street-Permits.jpg?mw=1920")
     );
+<<<<<<< HEAD
     private List<ClarifaiOutput<Concept>> result = request.executeSync().get();
     public color(String images){
         image = images;
@@ -29,3 +29,7 @@ public class color {
 
     }
 }
+=======
+    List<ClarifaiOutput<Concept>> result = request.executeSync().get();
+}
+>>>>>>> parent of e979af3... Added Anthony's changes to color
