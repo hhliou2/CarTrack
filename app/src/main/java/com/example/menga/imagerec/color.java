1179f9999504e8d827a25e8767000fb139219646
ame <33564712+ame@users.noreply.github.com>
2018-10-13 09:49:25 -0700

    commit color and entry

--- app/src/main/java/com/example/menga/imagerec/color.java
+++ app/src/main/java/com/example/menga/imagerec/color.java
@@ -1,5 +1,6 @@
 package com.example.menga.cartrack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
@@ -8,26 +9,33 @@ import clarifai2.api.request.model.PredictRequest;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.Model;
 import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Color;
 import clarifai2.dto.prediction.Concept;
 import clarifai2.dto.prediction.Prediction;
-
 public class color {
-    private String img="https://i.imgflip.com/vh6to.jpg";
-    final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
-    Model<Concept> generalModel = client.getDefaultModels().generalModel();
+    public ArrayList<entry> getData(String img){
+        final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
+        Model<Color> generalModel = client.getDefaultModels().colorModel();
 
-    PredictRequest<Concept> request = generalModel.predict().withInputs(
-            ClarifaiInput.forImage(img)
-    );
-    private List<ClarifaiOutput<Concept>> result = request.executeSync().get();
-    public List getList(){return result;}
+        PredictRequest<Color> request = generalModel.predict().withInputs(
+                ClarifaiInput.forImage(img)
+        );
+        List<ClarifaiOutput<Color>> result = request.executeSync().get();
+        ClarifaiOutput temp = result.get(0);
+        List<Prediction> tempList = temp.data();
+        ArrayList<entry> resultList = new ArrayList<>();
+        for(int i =0;i<tempList.size();i++){
+            resultList.add(new entry(tempList.get(i).asColor().webSafeColorName(),
+                    tempList.get(i).asColor().value(),tempList.get(i).asColor().webSafeHex()));
+        }
+        return resultList;
+    }
+
     public static void main(String[] args){
         color colour = new color();
-        List<ClarifaiOutput<Concept>> colorList = colour.getList();
-        ClarifaiOutput temp = colorList.get(0);
-        List<Prediction> tempList = temp.data();
-        for(int i =0;i<tempList.size();i++){
-            System.out.println(tempList.get(i).asConcept().name());
+        ArrayList<entry> list = colour.getData("http://pfguru.com/wp-content/uploads/2015/08/Ex-Showroom-Price-And-On-Road-Price.jpg");
+        for(int i =0;i<list.size();i++){
+            System.out.println(list.get(i).getColor()+" "+list.get(i).getValue()+" "+list.get(i).getHex());
         }
     }
 }
