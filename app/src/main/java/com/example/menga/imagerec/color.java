18db3c402812994874a907d3899aa76112024403
HASAN\Hasan <liouhasan@gmail.com>
2018-10-13 10:08:53 -0700

    Revert "Update app/src/main/java/com/example/menga/imagerec/color.java"
    This reverts commit 8da09d99aa6e46b961d971fe3289bfa1ab0c70fb.

--- app/src/main/java/com/example/menga/imagerec/color.java
+++ app/src/main/java/com/example/menga/imagerec/color.java
@@ -1,6 +1,5 @@
 package com.example.menga.imagerec;
 
-<<<<<<< HEAD
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 
@@ -13,9 +12,6 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.IOException;
-=======
-import java.util.ArrayList;
->>>>>>> 7c9308ecd6ea586178fd103f1d43d46865bb21eb
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
@@ -24,15 +20,14 @@ import clarifai2.api.request.model.PredictRequest;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.Model;
 import clarifai2.dto.model.output.ClarifaiOutput;
-import clarifai2.dto.prediction.Color;
 import clarifai2.dto.prediction.Concept;
 import clarifai2.dto.prediction.Prediction;
-public class color {
-    public ArrayList<entry> getData(String img){
-        final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
-        Model<Color> generalModel = client.getDefaultModels().colorModel();
 
-<<<<<<< HEAD
+public class color {
+    private String img="https://i.imgflip.com/vh6to.jpg";
+    final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
+    Model<Concept> generalModel = client.getDefaultModels().generalModel();
+
     PredictRequest<Concept> request = generalModel.predict().withInputs(
             ClarifaiInput.forImage(img)
     );
@@ -72,27 +67,9 @@ public class color {
         color colour = new color();
         List<ClarifaiOutput<Concept>> colorList = colour.getList();
         ClarifaiOutput temp = colorList.get(0);
-=======
-        PredictRequest<Color> request = generalModel.predict().withInputs(
-                ClarifaiInput.forImage(img)
-        );
-        List<ClarifaiOutput<Color>> result = request.executeSync().get();
-        ClarifaiOutput temp = result.get(0);
->>>>>>> 7c9308ecd6ea586178fd103f1d43d46865bb21eb
         List<Prediction> tempList = temp.data();
-        ArrayList<entry> resultList = new ArrayList<>();
         for(int i =0;i<tempList.size();i++){
-            resultList.add(new entry(tempList.get(i).asColor().webSafeColorName(),
-                    tempList.get(i).asColor().value(),tempList.get(i).asColor().webSafeHex()));
-        }
-        return resultList;
-    }
-
-    public static void main(String[] args){
-        color colour = new color();
-        ArrayList<entry> list = colour.getData("http://pfguru.com/wp-content/uploads/2015/08/Ex-Showroom-Price-And-On-Road-Price.jpg");
-        for(int i =0;i<list.size();i++){
-            System.out.println(list.get(i).getColor()+" "+list.get(i).getValue()+" "+list.get(i).getHex());
+            System.out.println(tempList.get(i).asConcept().name());
         }
     }
 }
