e09cc29d2929816bae787cc20b6bb5b73c76101f
ame <33564712+ame@users.noreply.github.com>
2018-10-13 10:53:13 -0700

    Update app/src/main/java/com/example/menga/imagerec/colorSense.java

--- app/src/main/java/com/example/menga/imagerec/colorSense.java
+++ app/src/main/java/com/example/menga/imagerec/colorSense.java
@@ -1,4 +1,4 @@
-package com.example.menga.cartrack;
+package com.example.menga.imagerec;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +12,8 @@ import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Color;
 import clarifai2.dto.prediction.Concept;
 import clarifai2.dto.prediction.Prediction;
-public class color {
-    public ArrayList<entry> getData(String img){
+public class colorSense {
+    public ArrayList<com.example.menga.cartrack.entry> getData(String img){
         final ClarifaiClient client = new ClarifaiBuilder("b6e11b6479da4bb5b33267988ab7aa4b").buildSync();
         Model<Color> generalModel = client.getDefaultModels().colorModel();
 
@@ -23,9 +23,9 @@ public class color {
         List<ClarifaiOutput<Color>> result = request.executeSync().get();
         ClarifaiOutput temp = result.get(0);
         List<Prediction> tempList = temp.data();
-        ArrayList<entry> resultList = new ArrayList<>();
+        ArrayList<com.example.menga.cartrack.entry> resultList = new ArrayList<>();
         for(int i =0;i<tempList.size();i++){
-            resultList.add(new entry(tempList.get(i).asColor().webSafeColorName(),
+            resultList.add(new com.example.menga.cartrack.entry(tempList.get(i).asColor().webSafeColorName(),
                     tempList.get(i).asColor().value(),tempList.get(i).asColor().webSafeHex()));
         }
         return resultList;
@@ -33,7 +33,7 @@ public class color {
 
     public static void main(String[] args){
         color colour = new color();
-        ArrayList<entry> list = colour.getData("http://pfguru.com/wp-content/uploads/2015/08/Ex-Showroom-Price-And-On-Road-Price.jpg");
+        ArrayList<com.example.menga.cartrack.entry> list = colour.getData("http://pfguru.com/wp-content/uploads/2015/08/Ex-Showroom-Price-And-On-Road-Price.jpg");
         for(int i =0;i<list.size();i++){
             System.out.println(list.get(i).getColor()+" "+list.get(i).getValue()+" "+list.get(i).getHex());
         }
