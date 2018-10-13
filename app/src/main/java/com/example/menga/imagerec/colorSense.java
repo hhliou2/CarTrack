a7dd5efecbb2fe8a47bf30c557f1d7ed61ed72b7
ame <33564712+ame@users.noreply.github.com>
2018-10-13 10:56:27 -0700

    rip

--- app/src/main/java/com/example/menga/imagerec/colorSense.java
+++ app/src/main/java/com/example/menga/imagerec/colorSense.java
@@ -16,6 +16,7 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
